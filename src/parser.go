0d7a17a0f9ee733605a0e76bfebffdeca069d66c
artuno <manikandanarvind@gmail.com>
2021-07-04 13:40:50 +0530

    Added structs

--- src/parser.go
+++ src/parser.go
@@ -5,8 +5,37 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"reflect"
+	"time"
 )
+
+type ParticipantOI struct {
+	future_index_long int
+	future_index_short int
+
+	future_stock_long int
+	future_stock_short int
+
+	option_index_call_long int
+	option_index_put_long int
+
+	option_index_call_short int
+	option_index_put_short int
+
+	option_stock_call_long int
+	option_stock_put_long int
+
+	option_stock_call_short int
+	option_stock_put_short int
+}
+
+type MarketwideOI struct {
+	position_date time.Time
+
+	client ParticipantOI
+	dii ParticipantOI
+	fii ParticipantOI
+	pro ParticipantOI
+}
 
 func parseOIData(filePath string) [][]string {
 	file, err := os.Open(filePath)
@@ -38,6 +67,9 @@ func main() {
 	fileList, _ := datasetDir.Readdirnames(0)
 	for _, name := range fileList {
 		records := parseOIData("./sample_dataset/" + name)
+
+
+
 		fmt.Println(records);
 	}
 }
